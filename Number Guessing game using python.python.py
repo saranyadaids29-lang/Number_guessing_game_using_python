73a3a0df2c1030abd8f2127b77bf5445a3e6a63b
Saranya Dellibabu <saranya.d.aids29@princedrkvasudevan.com>
2026-03-02 10:33:12 +0530

    Add files via upload

---
+++ Number Guessing game using python.python.py
@@ -0,0 +1,14 @@
+import random
+number = random.randint(1, 100)
+attempts = 0
+print("Welcome to Number Guessing Game")
+print("Guess a number between 1 and 100")
+while True:
+    guess=int(input("Enter the number: "))
+    attempts+=1
+    if(guess>number):
+        print("Too Long")
+    elif(guess<number):
+        print("Too Short")
+    else:
+        print("Correct! You guessed the number in", attempts, "attempts.")
